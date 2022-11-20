f5295bfa104cc60a88aef9f8a2c141d55b8c4d57
poo <pbrenna12@gmail.com>
2022-11-20 13:55:34 +0800

    deployed post and event-bus with k8s

--- toyProject/event-bus/index.js
+++ toyProject/event-bus/index.js
@@ -1,20 +1,21 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const axios = require("axios");
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 
 const events = [];
 
-app.post("/events", (req, res) => {
+app.post('/events', (req, res) => {
   const event = req.body;
 
   events.push(event);
 
-  axios.post("http://localhost:4011/events", event).catch((err) => {
+  axios.post('http://posts-clusterip-srv:4011/events', event).catch((err) => {
     console.log(err.message);
   });
+  /*
   axios.post("http://localhost:4012/events", event).catch((err) => {
     console.log(err.message);
   });
@@ -24,14 +25,15 @@ app.post("/events", (req, res) => {
   axios.post("http://localhost:4014/events", event).catch((err) => {
     console.log(err.message);
   });
+  */
 
-  res.send({ status: "OK" });
+  res.send({ status: 'OK' });
 });
 
-app.get("/events", (req, res) => {
+app.get('/events', (req, res) => {
   res.send(events);
 });
 
 app.listen(4005, () => {
-  console.log("Listening on 4005");
+  console.log('Listening on 4005');
 });
--- toyProject/posts/index.js
+++ toyProject/posts/index.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const { randomBytes } = require("crypto");
-const cors = require("cors");
-const axios = require("axios");
-const { type } = require("os");
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
+const { type } = require('os');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,28 +11,28 @@ app.use(cors());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
+app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
-  const id = randomBytes(4).toString("hex");
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
   const { title } = req.body;
   posts[id] = {
     id,
     title,
   };
 
-  axios.post("http://localhost:4005/events", {
-    type: "PostCreated",
+  axios.post('http://event-bus-srv:4005/events', {
+    type: 'PostCreated',
     data: posts[id],
   });
 
   res.status(201).send(posts[id]);
 });
 
-app.post("/events", (req, res) => {
-  console.log("Received Event:", req.body.type);
+app.post('/events', (req, res) => {
+  console.log('Received Event:', req.body.type);
 
   const { type, data } = req.body;
 
@@ -40,5 +40,5 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4011, () => {
-  console.log("Listening on 4011.");
+  console.log('Listening on 4011.');
 });
