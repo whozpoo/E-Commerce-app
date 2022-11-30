07cf7e49108b54ccbfb6e297f0b344c707a2c603
poo <pbrenna12@gmail.com>
2022-11-30 23:51:33 +0800

    implemented signup and signin

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import jwt from 'jsonwebtoken';
+
+import { validateRequest } from '../middlewares/validate-request';
 import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
@@ -14,12 +15,8 @@ router.post(
     .trim()
     .isLength({ min: 8, max: 20 })
     .withMessage('Password must be between 8 and 20 characters'),
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
