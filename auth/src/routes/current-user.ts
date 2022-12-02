7512aae025762da7c5e27d73a4764121220c0eb1
poo <pbrenna12@gmail.com>
2022-12-02 14:25:01 +0800

    refactor some common middlewares into a npm package

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@itemseller/commonlib';
 
 const router = express.Router();
 
